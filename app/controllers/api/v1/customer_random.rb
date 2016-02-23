4785f627cc7059691b87b6f2fffca68af7b35b47
Steve Pentler <stevepentler@gmail.com>
2016-02-22 19:41:40 -0700

    Reformats routes to default to JSON

---
+++ app/controllers/api/v1/customer_random.rb
@@ -0,0 +1,4 @@
+class Api::V1::CustomerRandomController < Api::ApiController
+  respond_to :json
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
-      get '/customers/find', to: 'customer_finder#show', defaults: {format: :json}
-      get '/customers/find_all', to: 'customer_finder#index', defaults: {format: :json}
-      resources :customers, only: [:index, :show], defaults: {format: :json}
-      resources :merchants, only: [:index, :show], defaults: {format: :json}
-      resources :items, only: [:index, :show], defaults: {format: :json}
-      resources :invoices, only: [:index, :show], defaults: {format: :json}
-      resources :invoice_items, only: [:index, :show], defaults: {format: :json}
-      resources :transactions, only: [:index, :show], defaults: {format: :json}
+    namespace :v1, defaults: {format: :json} do
+      get '/customers/random', to: 'customer_random#show'
+      get '/customers/find', to: 'customer_finder#show'
+      get '/customers/find_all', to: 'customer_finder#index'
+      resources :customers, only: [:index, :show]
+      resources :merchants, only: [:index, :show]
+      resources :items, only: [:index, :show]
+      resources :invoices, only: [:index, :show]
+      resources :invoice_items, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
     end
   end
 end
