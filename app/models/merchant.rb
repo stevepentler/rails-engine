5ccde49f2aef3723d335d896baee78a609a6aa1f
Steve Pentler <stevepentler@gmail.com>
2016-02-22 18:12:13 -0700

    Adds Customer index route and action

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,8 @@
 class Merchant < ActiveRecord::Base
   has_many :invoices
   has_many :items
+
+  def self.parse_parameters(params)
+    binding.pry
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, defaults: {format: :json}
+      resources :merchants, only: [:index, :show], defaults: {format: :json}
+      resources :customers, only: [:index, :show], defaults: {format: :json}
     end
   end
 end
