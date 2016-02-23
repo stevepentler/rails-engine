20b0a4742bdd17d8fa46eb4aa1b7dacb2c576203
Steve Pentler <stevepentler@gmail.com>
2016-02-22 18:15:56 -0700

    Adds Index and Show for Item

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::ItemsController < Api::ApplicationController
+  respond_to :json
+  def index
+    respond_with Item.all
+  end 
+
+  def show
+    respond_with Item.find(params[:id])
+  end
+end
