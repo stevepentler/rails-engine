0584bf7cc0733a29c8041f4f32b73adc9635f97f
Steve Pentler <stevepentler@gmail.com>
2016-02-23 15:23:26 -0700

    Passes Invoice Associated Invoice Items Test Harness

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
       get '/items/find_all', to: 'items_finder#index'
       resources :items, only: [:index, :show]
       get '/invoices/:id/transactions', to: 'invoice_transactions#index'
+      get '/invoices/:id/invoice_items', to: 'invoice_associated_invoice_items#index'
       get '/invoices/:id/items', to: 'invoice_associated_items#index'
       get '/invoices/random', to: 'invoices_random#show'
       get '/invoices/find', to: 'invoices_finder#show'
