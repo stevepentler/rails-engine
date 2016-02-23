397e08bbbcba292381b507e82a6eca564cacbfce
Steve Pentler <stevepentler@gmail.com>
2016-02-22 19:36:28 -0700

    find and find_all working

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/customers/find', to: 'customer_finder#show', defaults: {format: :json}
+      get '/customers/find_all', to: 'customer_finder#index', defaults: {format: :json}
       resources :customers, only: [:index, :show], defaults: {format: :json}
       resources :merchants, only: [:index, :show], defaults: {format: :json}
       resources :items, only: [:index, :show], defaults: {format: :json}
