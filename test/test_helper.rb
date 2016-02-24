3968c3574c131273cf9a43a918fc6a0748e71c65
Steve Pentler <stevepentler@gmail.com>
2016-02-24 09:53:43 -0700

    Adjusts to SimpleCov Rails, 98.96% covered

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,20 +1,15 @@
+require 'simplecov'
+SimpleCov.start "rails"
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'database_cleaner'
 require 'minitest/pride'
-require 'simplecov'
 
-SimpleCov.start
 
 class ActiveSupport::TestCase
   include FactoryGirl::Syntax::Methods
   fixtures :all
 
-  # def teardown
-  #   DatabaseCleaner.clean
-  #   FactoryGirl.reload
-  # end
-  
 end
 
