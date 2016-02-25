2998289eb4228b250bde7539bf869481f271a2c7
Steve Pentler <stevepentler@gmail.com>
2016-02-24 23:19:55 -0700

    Deepens customer relationship tests

--- test/controllers/customers/invoices_controller_test.rb
+++ test/controllers/customers/invoices_controller_test.rb
@@ -3,11 +3,21 @@ require 'test_helper'
 class Api::V1::Customers::InvoicesControllerTest < ActionController::TestCase
   test "#index" do 
     customer = create(:customer)
+    merchant = create(:merchant)
+
+    10.times do 
+      Invoice.create(customer_id: customer.id,
+                     merchant_id: merchant.id,
+                     status: "shipped")
+    end
     
     get :index, format: :json, id: customer.id
 
-    invoices = JSON.parse(response.body)
+    result = JSON.parse(response.body)
 
     assert_response :success
+    assert_equal 10, result.count
+    assert_equal customer.id, result.first["customer_id"]
+    assert_equal merchant.id, result.first["merchant_id"]
   end
 end
--- test/controllers/customers/transactions_controller_test.rb
+++ test/controllers/customers/transactions_controller_test.rb
@@ -3,11 +3,21 @@ require 'test_helper'
 class Api::V1::Customers::TransactionsControllerTest < ActionController::TestCase
   test "#index" do 
     customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = Invoice.create(customer_id: customer.id,
+                             merchant_id: merchant.id,
+                             status: "shipped")
+
+    10.times do 
+      Transaction.create(invoice_id: invoice.id)
+    end
     
     get :index, format: :json, id: customer.id
 
-    invoices = JSON.parse(response.body)
+    result = JSON.parse(response.body)
 
     assert_response :success
+    assert_equal 10, result.count
+    assert_equal invoice.id, result.first["invoice_id"]
   end
 end
