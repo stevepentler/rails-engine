3eb7ae81e0a0dea3ac941d361bee4759804b0c06
Steve Pentler <stevepentler@gmail.com>
2016-02-24 22:12:12 -0700

    Adds total revenue by data test for merchants controller

--- test/controllers/merchants/revenue_controller_test.rb
+++ test/controllers/merchants/revenue_controller_test.rb
@@ -13,8 +13,7 @@ class Api::V1::Merchants::RevenueControllerTest < ActionController::TestCase
 
     10.times do 
       Transaction.create(invoice_id: invoice.id, 
-                         result: "success",
-                         created_at: time)
+                         result: "success")
     end
 
     item = Item.create(name: "boomerang",
@@ -26,8 +25,7 @@ class Api::V1::Merchants::RevenueControllerTest < ActionController::TestCase
       InvoiceItem.create(item_id: item.id,
                          invoice_id: invoice.id,
                          quantity: 1,
-                         unit_price: 1,
-                         created_at: time)
+                         unit_price: 1)
     end
 
     get :show, format: :json, id: merchant.id
@@ -37,5 +35,43 @@ class Api::V1::Merchants::RevenueControllerTest < ActionController::TestCase
     assert_response :success
 
     assert_equal "100.0", result["revenue"]
+    # $1* 10 InvoiceItems * 10 Transactions
+  end
+
+  test "#show revenue for merchant by date" do
+    time = Time.now
+    customer = create(:customer)
+    merchant= create(:merchant)
+
+    invoice = Invoice.create(customer_id: customer.id, 
+                             merchant_id: merchant.id, 
+                             status: "shipped",
+                             created_at: time)
+
+    10.times do 
+      Transaction.create(invoice_id: invoice.id, 
+                         result: "success")
+    end
+
+    item = Item.create(name: "boomerang",
+                       description: "flies",
+                       unit_price: 1,
+                       merchant_id: merchant.id)
+
+    5.times do 
+      InvoiceItem.create(item_id: item.id,
+                         invoice_id: invoice.id,
+                         quantity: 1,
+                         unit_price: 1)
+    end
+
+    get :show, format: :json, id: merchant.id, date: time
+
+    result = JSON.parse(response.body)
+    
+    assert_response :success
+
+    assert_equal "50.0", result["revenue"]
+    # $1* 5 InvoiceItems * 10 Transactions
   end
 end
