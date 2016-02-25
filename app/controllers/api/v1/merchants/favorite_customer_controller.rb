2574903f56effd3ad096052fad87b0e253363462
Steve Pentler <stevepentler@gmail.com>
2016-02-24 18:49:35 -0700

    refactors merchant's favorite customer to customer model

--- app/controllers/api/v1/merchants/favorite_customer_controller.rb
+++ app/controllers/api/v1/merchants/favorite_customer_controller.rb
@@ -2,17 +2,18 @@ class Api::V1::Merchants::FavoriteCustomerController < Api::ApiController
   respond_to :json
 
   def show
-    respond_with Customer.find(favorite_customer)
+    customer = Customer.new
+    respond_with Customer.find(customer.favorite_customer(params))
   end
 
   private 
 
-  def favorite_customer
-    merchant = Merchant.find(params[:id])
-    customer_id = merchant.invoices.joins(:transactions)
-                     .where("result = 'success'") 
-                     .group(:customer_id)
-                     .count
-                     .max_by{|k,v| v}.first
-  end
+  # def favorite_customer
+  #   merchant = Merchant.find(params[:id])
+  #   customer_id = merchant.invoices.joins(:transactions)
+  #                    .where("result = 'success'") 
+  #                    .group(:customer_id)
+  #                    .count
+  #                    .max_by{|k,v| v}.first
+  # end
 end
