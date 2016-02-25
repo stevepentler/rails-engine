22645e69e1642c15111f351fc7f7ac1f06a2e815
Steve Pentler <stevepentler@gmail.com>
2016-02-25 00:15:24 -0700

    Refactors item tests

--- test/controllers/items/invoice_items_controller_test.rb
+++ test/controllers/items/invoice_items_controller_test.rb
@@ -3,11 +3,15 @@ require 'test_helper'
 class Api::V1::Items::InvoiceItemsControllerTest < ActionController::TestCase
   test "#index" do 
     item = create(:item)
+    invoice_item = InvoiceItem.create(item_id: item.id)
     
     get :index, format: :json, id: item.id
 
-    invoice_items = JSON.parse(response.body)
+    result = JSON.parse(response.body)
 
     assert_response :success
+
+    assert_equal invoice_item.id, result.first["id"]
+    assert_equal invoice_item.item_id, result.first["item_id"]
   end
 end
--- test/controllers/items/merchant_controller_test.rb
+++ test/controllers/items/merchant_controller_test.rb
@@ -2,12 +2,15 @@ require 'test_helper'
 
 class Api::V1::Items::MerchantControllerTest < ActionController::TestCase
   test "#show" do 
-    item = create(:item)
+    merchant = create(:merchant)
+    item = Item.create(merchant_id: merchant.id)
     
     get :show, format: :json, id: item.id
 
-    merchant = JSON.parse(response.body)
+    result = JSON.parse(response.body)
 
     assert_response :success
+    assert_equal merchant.id, result["id"]
+    assert_equal merchant.name, result["name"]
   end
 end
